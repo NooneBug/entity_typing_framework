797e0e246898b747b608e4b132fea81be6eb0201
NooneBug <manuel.vimercati@gmail.com>
2021-12-29 13:16:53 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,12 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
-test_requirements = [ ]
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest>=3', ]
 
 setup(
     author="Manuel Vimercati",
