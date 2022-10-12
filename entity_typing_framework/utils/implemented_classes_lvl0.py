37d0530f1304f8c51821c3335a1f328369c69e48
Christian Bernasconi <christian.bernasconi96@gmail.com>
2022-10-12 10:43:34 +0200

    asd

--- entity_typing_framework/utils/implemented_classes_lvl0.py
+++ entity_typing_framework/utils/implemented_classes_lvl0.py
@@ -1,10 +1,10 @@
 from entity_typing_framework.EntityTypingNetwork_classes.base_network import BaseEntityTypingNetwork, IncrementalEntityTypingNetwork
 from entity_typing_framework.dataset_classes.large_datasets.large_datasets_for_dataloaders import BERT_ET_DatasetLarge, ELMo_ET_DatasetLarge, ET_DatasetLarge
 from entity_typing_framework.dataset_classes.large_datasets.large_tokenized_datasets import BaseBERTTokenizedDatasetLarge, ELMoTokenizedDatasetLarge
-from entity_typing_framework.dataset_classes.tokenized_datasets import BaseBERTTokenizedDataset, ELMoTokenizedDataset
+from entity_typing_framework.dataset_classes.tokenized_datasets import BaseBERTTokenizedDataset, ELMoTokenizedDataset, GloVeTokenizedDataset
 from entity_typing_framework.main_module.NFETC_loss_modules.nfetc_loss_modules import BCENFETCCustomLossModule, BCENFETCLossModule
 from entity_typing_framework.main_module.inference_manager import BaseInferenceManager, FlatToHierarchyThresholdOrMaxInferenceManager, IncrementalThresholdOrMaxInferenceManager, MaxInferenceManager, ThresholdOrMaxInferenceManager
-from entity_typing_framework.main_module.losses_modules import BCELossModule, FlatBCELossModule, FlatRankingLossModule, RankingLossModule
+from entity_typing_framework.main_module.losses_modules import BCELossModule, FlatBCELossModule, FlatRankingLossModule, RankingLossModule, WeightedBCELossModule
 from entity_typing_framework.main_module.KENN_loss_modules.kenn_loss_modules import BCEMultiLossModule
 from torch.utils.data.dataloader import DataLoader
 from entity_typing_framework.dataset_classes.datasets_for_dataloader import ELMo_ET_Dataset, ET_Dataset
@@ -29,6 +29,7 @@ IMPLEMENTED_CLASSES_LVL0 = {
     'RankingLossModule' : RankingLossModule,
     'FlatBCELossModule' : FlatBCELossModule,
     'FlatRankingLossModule' : FlatRankingLossModule,
+    'WeightedBCELossModule' : WeightedBCELossModule,
     'ELMoTokenizedDataset' : ELMoTokenizedDataset,
     'ELMo_ET_Dataset' : ELMo_ET_Dataset,
     'ET_DatasetLarge' : ET_DatasetLarge,
